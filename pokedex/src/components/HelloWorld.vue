7f1810098f235f11375208fb750e6d4ca6552ed4
maxime-louis14 <maximelouis321@gmail.com>
2022-05-21 16:10:16 +0200

    commit

---
+++ pokedex/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <div :key="index" v-for="(pokemon, index) in pokemon">
+      <h1>{{ pokemon.name }}</h1>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "HelloWorld",
+
+  data(){
+
+    return{
+      pokemon: null,
+    }
+  },
+
+  mounted() {
+    axios
+    .get('https://pokeapi.co/api/v2/')
+    .then((reponse) => {
+      this.pokemon = reponse.data;
+      console.log(this.pokemon)
+    });
+  }
+};
+</script>
+
+
+<style scoped>
+
+</style>
